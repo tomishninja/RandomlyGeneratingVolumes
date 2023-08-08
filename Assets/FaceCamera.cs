da0decccf60853aa21ce3b418f3652677ad22889
tomishninja <tomishninja@users.noreply.github.com>
2023-08-08 12:11:16 +0930

    Changes for Video

--- Assets/FaceCamera.cs
+++ Assets/FaceCamera.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class FaceCamera : MonoBehaviour
 {
+    [SerializeField] private float rotationSpeed = 5f; // Controls the rotation speed/lag
+    [SerializeField] private float smoothingFactor = 5f;
+
+    private Transform mainCameraTransform;
+    private Quaternion desiredRotation; // Used for damping
+
     // Update is called once per frame
     void Update()
     {
         // Get the main camera in the scene
-        Camera mainCamera = Camera.main;
+        if (mainCameraTransform == null)
+            mainCameraTransform = Camera.main.transform;
 
-        if (mainCamera != null)
+        if (mainCameraTransform != null)
         {
             // Calculate the direction from the object to the main camera
-            Vector3 lookDirection = mainCamera.transform.position - transform.position;
+            Vector3 lookDirection = mainCameraTransform.position - transform.position;
             lookDirection = -lookDirection;
 
-            // Rotate the object's transform to face the camera
-            transform.rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+            // Filter noise by smoothing the lookDirection vector (optional)
+            lookDirection = Vector3.Lerp(transform.forward, lookDirection, Time.deltaTime * smoothingFactor);
+
+            // Calculate the desired rotation to face the camera
+            desiredRotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+
+            // Apply rotation with lag using spherical linear interpolation (slerp)
+            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, Time.deltaTime * rotationSpeed);
         }
         else
         {
